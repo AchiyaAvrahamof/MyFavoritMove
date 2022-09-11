d6f737e628bd5e35f1352dafb3081cd85b777b1f
Achiya Avrahamof <achiyaav001@gmail.com>
2022-09-11 17:12:27 +0300

    first

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,64 @@
+import "../mymovie/pages/rating.css"
+import { useState } from "react";
+import {Rating} from '@mui/material'
+import { Icon } from '@mui/material';
+
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+const MainPage = (props)=>{
+    const [isOpen,setIsOpen]=useState(false)
+    const[counter,setCounter]=useState(props.rating)
+    
+    const removeMovie = () => {
+        props.MovieesList.splice(props.index, 1)
+        props.setMovieList([...props.MovieesList])
+        console.log(props.MovieesList);
+        
+      };
+      
+    
+    
+    
+    return(
+        <div>
+
+        <div className="movieContiner" >
+            
+            <div className="movieSection">
+            <img src={props.img} className="movieimg" onClick={()=> setIsOpen(!isOpen)}></img>
+            </div>
+            <div className="movieSection nameMovie">
+            <h4 >{props.title}</h4>
+            </div>
+            
+            <div className="movieSection rating">
+            <Rating name="size-medium" defaultValue={counter} onChange={((event, newValue)=> setCounter(newValue))} />
+
+            </div>
+            <div className="movieSection">
+            
+            <DeleteForeverIcon onClick={()=> {removeMovie()}} className="remove"></DeleteForeverIcon>
+            </div>
+            { isOpen?(
+                
+                <div>
+           
+                <iframe width="250" height="250" src={props.trailer} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            ):
+            (
+                <div></div>
+            )
+
+        }
+
+            </div>
+        
+            
+        </div>  
+    )
+    
+    } 
+
+
+export default MainPage
+
---
+++ src/mymovie/pages/rating.jsx
@@ -0,0 +1,85 @@
+import { Children, useState } from "react";
+import MainPage from "../../components/MainPage";
+import "./rating.css"
+import { Button, TextField } from '@mui/material'
+const Rating = () =>{
+    let title,img,rating,trailer;
+const [isOpen,setIsOpen]=useState(false)
+  const [MovieesList,setMovieList] = useState([
+    {
+        
+        img:"https://bit.ly/3wD6xXv",
+        title:"Rio",
+        rating:4,
+        trailer:"https://www.youtube.com/embed/mwbYoGxEe9g",
+    },
+    {
+        
+        img:"https://bit.ly/3PX63Cz",
+        title:"Harry potter",
+        rating:0,
+        trailer:"https://www.youtube.com/embed/Cj6FmLV1XaQ",
+    }
+])
+   
+   const addNewMovieArray=(movie)=>{
+    const NewMovieArray= [...MovieesList]
+    NewMovieArray.push(movie)
+    setMovieList(NewMovieArray)
+   }
+   
+   
+    
+  return(
+      //html
+      <div >
+        <div className="addNewMbtm">
+        <Button variant="contained" disableElevation onClick={()=> setIsOpen(!isOpen)} className="button-51">Add new movie</Button>
+       
+        </div>
+        { isOpen?(
+
+            <div className="NewMovieSection">
+            <TextField id="outlined-basic" label="Title" variant="outlined" className="newS" onChange={(e)=> title=e.target.value}/>
+            <TextField id="outlined-basic" label="Image" variant="outlined" className="newS" onChange={(e)=> img=e.target.value}/>
+            <TextField id="outlined-basic" label="Rating" variant="outlined" className="newS" onChange={(e)=> rating=e.target.value}/>
+            <TextField id="outlined-basic" label="Trailer" variant="outlined" className="newS" onChange={(e)=> trailer=e.target.value}/>
+             
+            
+                <Button variant="contained" color="success"
+                onClick={() =>{
+                
+                    addNewMovieArray({img:img,
+                        title:title,
+                        rating:rating,
+                        trailer:trailer,})
+                        setIsOpen(!isOpen)
+                
+                }} className="button-62">Save</Button>
+            </div>
+            ):
+            (
+                <div></div>
+            )
+
+        }
+        {MovieesList.map((movie,index) =>{
+        return <div key={index}>
+            <MainPage
+            img={movie.img}
+            title={movie.title}
+            rating={movie.rating}
+            trailer={movie.trailer}
+            index={index}
+            MovieesList={MovieesList}
+            setMovieList={setMovieList}
+            ></MainPage>
+            
+         </div>
+        })}
+      
+      </div>
+  )
+  }
+  
+  export default Rating;    
